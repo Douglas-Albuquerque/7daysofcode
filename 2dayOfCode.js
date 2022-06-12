64095f67f2520ba0b7e9da34b624c507f93a39f1
douglas-albuquerque <douglasalbuquerq@gmail.com>
2022-06-12 13:52:42 -0300

    Adição de arquivos no Git

---
+++ 2dayOfCode.js
@@ -0,0 +1,15 @@
+const nome = prompt("Qual seu nome?");
+const idade = prompt("Qual sua idade?");
+const linguagem = prompt("Qual linguagem de programação você esta estudando?");
+const msg = `"Olá ${nome}, você tem ${idade} anos e já esta aprendendo ${linguagem}!"`;
+
+alert(msg);
+
+const gosta = prompt(`Você gosta de estudar ${linguagem}? Resposta com o numero 1 - SIM ou 2 - NÃo`);
+if(gosta == 1){
+    alert("Muito bom! Continue estudando e você terá muito sucesso!");
+}
+if(gosta == 2){
+    alert("Ahh que pena... Já tentou aprender outras linguagens?");
+}
+//${linguagem} >> lembrete isto é uma placeholder de uma template string que são os acentos agudos >> ``.
